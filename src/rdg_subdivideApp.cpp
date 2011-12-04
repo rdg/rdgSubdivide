1c3db523c4b6e327d676d1114c95a7d3eac4a20a
Georg Duemlein <rdg@preset.de>
2011-12-04 23:04:22 +1300

    ADD: simple random frame archival feature

--- src/rdg_subdivideApp.cpp
+++ src/rdg_subdivideApp.cpp
@@ -1,6 +1,9 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Camera.h"
+#include "cinder/Rand.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Utilities.h"
 
 #include "Manager.h"
 #include "Element.h"
@@ -10,6 +13,7 @@ using namespace ci::app;
 using namespace std;
 
 bool		RUN = false;
+bool		ARCHIVE = false;
 
 class rdg_subdivideApp : public AppBasic {
   public:
@@ -21,17 +25,22 @@ class rdg_subdivideApp : public AppBasic {
 	
 	CameraPersp		mCam;
 	Manager mgmt;
+	
+	int mCurrentFrame;
+	float archiver;
 };
 
 
 
 void rdg_subdivideApp::setup()
 {
-	setWindowSize( 1440, 568 );
-	
+	//setWindowSize( 1440, 568 );
+	setWindowSize( 720, 274 );
 	mCam = CameraPersp( getWindowWidth(), getWindowHeight(), 40.0f );
 	mCam.setPerspective( 40.0f, getWindowAspectRatio(), 0.1f, 60.0f );
 	mCam.lookAt( Vec3f( 0, 12.5f,  0 ), Vec3f( 0, 0, 0 ) );
+	
+	mCurrentFrame = 0;
 }
 
 void rdg_subdivideApp::mouseDown( MouseEvent event )
@@ -60,6 +69,10 @@ void rdg_subdivideApp::keyDown( KeyEvent event )
 		Vec3f eye = mCam.getEyePoint();
 		mCam.setEyePoint( eye - Vec3f( 0, 5, 0 ) );
 	}
+	
+	if( ( key == 'a' ) || ( key == 'A' ) )
+		ARCHIVE = ! ARCHIVE;
+	
 }
 	
 void rdg_subdivideApp::update()
@@ -78,12 +91,22 @@ void rdg_subdivideApp::draw()
  
 	gl::setMatrices( mCam );
 	
-	if( RUN) {
+	if( RUN ) {
 		mgmt.exists();
 		mgmt.trigger();
 	}
 	
 	mgmt.render();
+	if( ARCHIVE )
+	{
+		archiver = Rand::randFloat();
+		if ( archiver > 0.95f && archiver < 0.97f )
+		{
+			// "cinder" + getPathSeparator()
+			writeImage( getHomeDirectory() + "rdgSubdivide." + toString( mCurrentFrame ) + ".png", copyWindowSurface() );
+			mCurrentFrame++;
+		}
+	}
 }
 
 
